167afb1c865f4bb0f878289e887d3c3e9620b4d0
BlazingRockStorm <rocker.gryphon@gmail.com>
2025-03-15 01:41:33 +0900

    Add Mongoid integration and create Post model with basic CRUD structure

--- app.rb
+++ app.rb
@@ -1,9 +1,37 @@
+require './models/Post'
+
 class App < Roda
   plugin :json
+  plugin :all_verbs
 
   route do |r|
-    r.get "hello" do
-      { message: "Hello, world!" }
+    r.on "posts" do
+      r.get do
+        Post.all
+      end
+
+      # r.is String do |id|
+      #   r.get do
+      #     post = Post.find(id)
+      #     post || r.halt(404)
+      #   end
+
+      #   r.post do
+      #     # Handle POST /posts/$ID
+      #   end
+
+      #   r.put do
+      #     # Handle PUT /posts/$ID
+      #   end
+
+      #   r.patch do
+      #     # Handle PATCH /posts/$ID
+      #   end
+
+      #   r.delete do
+      #     # Handle DELETE /posts/$ID
+      #   end
+      # end
     end
   end
 end
